2da6d12d69614101684344af50e3b84f86b61681
Dag <birudagmawi@gmail.com>
2012-10-05 08:17:47 -0500

    Update old/includes/config.php

--- old/includes/config.php
+++ old/includes/config.php
@@ -1,9 +1,9 @@
 <?php
 
 // When the site does go live, this documented should me moved outside of www root. 
-define("CONSUMER_KEY","JqREbOjNdtCGcW2Pijzyyg");
-define("CONSUMER_SECRET","Jab8uPyIAXmyagwisrVFPFo6mHJaTjbyVOzzn3UwJg");
+define("CONSUMER_KEY","");
+define("CONSUMER_SECRET","");
 define("DB_NAME","atecx");
-define("OAUTH_CALLBACK","http://foxhound.privatedns.org/atecx/callback.php?awesome=yes")
+define("OAUTH_CALLBACK","")
 
 ?>
