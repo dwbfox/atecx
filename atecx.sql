348d19d4e195fb19d9a44858f751aa1c3169f618
foxhound22003 <foxhound22003@yahoo.com>
2012-10-15 21:23:18 -0500

    The database structure for ATEC SQL

---
+++ atecx.sql
@@ -0,0 +1,84 @@
+-- phpMyAdmin SQL Dump
+-- version 3.4.10.1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Oct 15, 2012 at 10:11 PM
+-- Server version: 5.1.61
+-- PHP Version: 5.2.9
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `chromefa_atecx`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `milestones`
+--
+
+CREATE TABLE IF NOT EXISTS `milestones` (
+  `milestone_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `added_date` date NOT NULL,
+  `description` int(11) NOT NULL,
+  `deadline_date` date NOT NULL,
+  PRIMARY KEY (`milestone_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `proficiencies`
+--
+
+CREATE TABLE IF NOT EXISTS `proficiencies` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `prof_id` int(11) DEFAULT NULL,
+  `prof_value` int(11) DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=13 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `prof_roles`
+--
+
+CREATE TABLE IF NOT EXISTS `prof_roles` (
+  `prof_id` int(11) NOT NULL,
+  `prof_name` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`prof_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `screen_name` varchar(45) NOT NULL,
+  `join_date` date NOT NULL,
+  `avatar` varchar(255) DEFAULT NULL,
+  `email` varchar(45) NOT NULL,
+  `bio` text,
+  `interests` text,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `screen_name` (`screen_name`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
